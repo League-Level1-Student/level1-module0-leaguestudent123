658e5e25592755a92964a043ef7605c128386a26
ilabuser <ilabuser@172.16.116.67>
2019-01-07 17:50:25 -0800

    houses

--- src/Houses.java
+++ src/Houses.java
@@ -1,13 +1,51 @@
 
 
+import java.awt.Color;
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 import org.jointheleague.graphical.robot.RobotImage;
 
 public class Houses {
+	
+	static Robot bob =new Robot();
+	static Random num=new Random();
 public static void main(String[] args) {
-	 Robot bob =new Robot();
+	 
+	 bob.setSpeed(10);
+	 bob.penDown();
 	 bob.moveTo(10,400);
 	 bob.turn(90);
+	 
+	 for (int i = 0; i < 10; i++) {
+		 drawHouse(num.nextInt(120));
+	}
+	
+}
+
+static void  drawHouse(int hight) {
+
+	 bob.setPenColor(Color.green);
 	 bob.move(50);
+	 bob.turn(-90);	
+	 bob.setRandomPenColor();
+	 bob.move(hight);
+	 if (num.nextInt(2)==0) {
+		bob.turn(90);
+		bob.move(20);
+		bob.turn(90);
+	}else{
+		bob.turn(45);
+		bob.move(10);
+		bob.turn(90);
+		bob.move(10);
+		bob.turn(45);
+	}
+	
+	 
+	 bob.move(hight);
+	 bob.turn(-90);
+	 
+	 
 }
 }
